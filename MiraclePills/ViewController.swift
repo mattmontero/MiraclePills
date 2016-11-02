7c6eba474789cf514038fcc5661d1f43c65a210f
Matthew Montero <matthewmontero@Matthews-MacBook-Pro.local>
2016-11-02 16:15:05 -0700

    Add Name, City, and State Fields with a Picker

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerButton: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "Alabama"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        statePicker.dataSource = self
+        statePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,6 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func stateButtonPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerButton.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 }
 
